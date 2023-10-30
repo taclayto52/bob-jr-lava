5d1cccbacc391b11ce210acd437829d3f32fc5de
Matthew Harrison <mcharrison7@gmail.com>
2023-10-29 21:01:48 -0400

    Extract functions from trackLoaded method

--- src/main/java/com/bob/jr/TrackScheduler.java
+++ src/main/java/com/bob/jr/TrackScheduler.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Stack;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -46,32 +47,15 @@ public class TrackScheduler implements AudioLoadResultHandler {
         updateTrackCache(track);
 
         synchronized (announcementTracks) {
-            final var announcementTrackKey = announcementTracks.keySet().stream()
-                    .filter(announcementKey -> announcementKey.contains(track.getIdentifier()))
-                    .findAny();
+            final var announcementTrackKey = getAnnouncementTrackKey(track);
+
             if (announcementTrackKey.isPresent()) {
                 final AnnouncementTrack announcementTrack = announcementTracks.get(announcementTrackKey.get());
 
-                // handle upcoming announcement
-                track.setPosition(Math.round(announcementTrack.getStartTime() * 1000));
-                final long setEndTime;
-                if (announcementTrack.getEndTime() == 0) {
-                    setEndTime = track.getDuration();
-                } else {
-                    setEndTime = Math.round(announcementTrack.getEndTime()) * 1000;
-                }
-                final TrackMarker trackMarker = new TrackMarker(setEndTime, (markerState) -> {
-                    if (markerState == TrackMarkerHandler.MarkerState.REACHED) {
-                        logger.debug("Marker has been reached");
-                    } else if (markerState == TrackMarkerHandler.MarkerState.ENDED) {
-                        logger.debug("Marker has ended");
-                    } else {
-                        logger.debug(String.format("reached unknown marker state: %s", markerState));
-                    }
-                    announcementTracks.remove(announcementTrack.getTrackUrl());
-                    announcementTrackPlayer.stopTrack();
-                    setDefaultTrackPlayerAsActive();
-                });
+                final long setEndTime = setTrackStartAndEndTime(track, announcementTrack);
+
+                final TrackMarker trackMarker = getTrackMarker(setEndTime, announcementTrack);
+
                 track.setMarker(trackMarker);
                 setAnnouncementTrackPlayerAsActive();
                 announcementTrackPlayer.playTrack(track);
@@ -79,6 +63,40 @@ public class TrackScheduler implements AudioLoadResultHandler {
                 playTrackWithDefaultTrackPlayer(track);
             }
         }
+    }
+
+    private TrackMarker getTrackMarker(long setEndTime, AnnouncementTrack announcementTrack) {
+        final TrackMarker trackMarker = new TrackMarker(setEndTime, (markerState) -> {
+            if (markerState == TrackMarkerHandler.MarkerState.REACHED) {
+                logger.debug("Marker has been reached");
+            } else if (markerState == TrackMarkerHandler.MarkerState.ENDED) {
+                logger.debug("Marker has ended");
+            } else {
+                logger.debug(String.format("reached unknown marker state: %s", markerState));
+            }
+            announcementTracks.remove(announcementTrack.getTrackUrl());
+            announcementTrackPlayer.stopTrack();
+            setDefaultTrackPlayerAsActive();
+        });
+        return trackMarker;
+    }
+
+    private static long setTrackStartAndEndTime(AudioTrack track, AnnouncementTrack announcementTrack) {
+        track.setPosition(Math.round(announcementTrack.getStartTime() * 1000));
+        final long setEndTime;
+        if (announcementTrack.getEndTime() == 0) {
+            setEndTime = track.getDuration();
+        } else {
+            setEndTime = Math.round(announcementTrack.getEndTime()) * 1000;
+        }
+        return setEndTime;
+    }
+
+    private Optional<String> getAnnouncementTrackKey(AudioTrack track) {
+        final var announcementTrackKey = announcementTracks.keySet().stream()
+                .filter(announcementKey -> announcementKey.contains(track.getIdentifier()))
+                .findAny();
+        return announcementTrackKey;
     }
 
     private void playTrackWithDefaultTrackPlayer(AudioTrack track) {
